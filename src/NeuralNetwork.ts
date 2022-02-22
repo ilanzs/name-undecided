fd0f1fb22992c29f32b174f1fd02abd44165392e
ilanzs <ilanzsegal@gmail.com>
2022-02-22 12:28:43 +0200

    Made the feedForward algorithm use multiple layers

--- src/NeuralNetwork.ts
+++ src/NeuralNetwork.ts
@@ -23,27 +23,27 @@ function squared(x: number) {
 
 export class NeuralNetwork {
   inputNum: number;
-  hiddenNum: number;
+  hiddenNums: number[];
   outputNum: number;
-  weightsIH: Matrix;
-  weightsHO: Matrix;
-  biasIH: Matrix;
-  biasHO: any;
+  weights: Matrix[] = [];
+  biases: Matrix[] = [];
   lr: number;
-  constructor(inNum: number, hidNum: number, outNum: number, lr: number) {
+  constructor(inNum: number, hidNums: number[], outNum: number, lr: number) {
     this.inputNum = inNum;
-    this.hiddenNum = hidNum;
+    this.hiddenNums = hidNums;
     this.outputNum = outNum;
 
-    this.weightsIH = new Matrix(this.hiddenNum, this.inputNum);
-    this.weightsIH.map(random);
-    this.weightsHO = new Matrix(this.outputNum, this.hiddenNum);
-    this.weightsHO.map(random);
+    // Initialize weights and biases
+    let layers = [this.inputNum, ...this.hiddenNums, this.outputNum];
+    for (let l = 0; l < layers.length - 1; l++) {
+      let w = new Matrix(layers[l + 1], layers[l]);
+      w.map(random);
+      this.weights.push(w);
 
-    this.biasIH = new Matrix(this.hiddenNum, 1);
-    this.biasIH.map(random);
-    this.biasHO = new Matrix(this.outputNum, 1);
-    this.biasHO.map(random);
+      let b = new Matrix(layers[l + 1], 1);
+      b.map(random);
+      this.biases.push(b);
+    }
 
     this.lr = lr;
   }
@@ -51,68 +51,14 @@ export class NeuralNetwork {
   feedForward(inputs: number[]) {
     // Convert inputs into a Matrix object
     const inputsMatrix = Matrix.fromArray(inputs, this.inputNum, 1);
+    let m: Matrix = inputsMatrix;
 
-    // Calculate the hidden layer output
-    const hiddenInputs = Matrix.mult(this.weightsIH, inputsMatrix);
-    const hiddenOutputs = Matrix.addMatrix(hiddenInputs, this.biasIH);
-    const hiddenOutputsSigmoid = Matrix.map(hiddenOutputs, sigmoid);
-
-    // Calculate the output layer output
-    const outputInputs = Matrix.mult(this.weightsHO, hiddenOutputsSigmoid);
-    const outputOutputs = Matrix.addMatrix(outputInputs, this.biasHO);
-    const outputOutputsSigmoid = Matrix.map(outputOutputs, sigmoid);
-    return outputOutputsSigmoid.toArray().reduce((accumulator, value) => accumulator.concat(value), []);
-  }
-
-  train(inputs: number[], targetsArr: number[]) {
-    // Convert inputs into a Matrix object
-    const inputsMatrix = Matrix.fromArray(inputs, this.inputNum, 1);
-
-    // Calculate the hidden layer output
-    const hiddenInputs = Matrix.mult(this.weightsIH, inputsMatrix);
-    const hiddenOutputs = Matrix.addMatrix(hiddenInputs, this.biasIH);
-    const hiddenOutputsSigmoid = Matrix.map(hiddenOutputs, sigmoid);
-
-    // Calculate the output layer output
-    const outputInputs = Matrix.mult(this.weightsHO, hiddenOutputsSigmoid);
-    const outputOutputs = Matrix.addMatrix(outputInputs, this.biasHO);
-    const outputOutputsSigmoid = Matrix.map(outputOutputs, sigmoid);
-
-    // Convert arrays to Matrix objects
-    const outputs = outputOutputsSigmoid;
-    const targets = Matrix.fromArray(targetsArr, this.outputNum, 1);
-
-    // Calculate the output errors
-    const outputErrors = Matrix.subMatrix(targets, outputs);
-
-    // Calculate the gradient of the output errors
-    const gradients = Matrix.map(outputs, dsigmoid);
-    gradients.hadamard(outputErrors);
-    gradients.scale(this.lr);
-    const hiddenTransposed = Matrix.transpose(hiddenOutputsSigmoid);
-    const weightsHODeltas = Matrix.mult(gradients, hiddenTransposed);
-
-    // Update the weights of the output layer
-    this.weightsHO.addMatrix(weightsHODeltas);
-
-    // Update the biases of the outpu layer
-    this.biasHO.addMatrix(gradients);
-
-    // Calculate the hidden errors
-    const weightsHOTransposed = Matrix.transpose(this.weightsHO);
-    const hiddenErrors = Matrix.mult(weightsHOTransposed, outputErrors);
-
-    // Calculate the gradient of the hidden errors
-    const hiddenGradients = Matrix.map(hiddenOutputsSigmoid, dsigmoid);
-    hiddenGradients.hadamard(hiddenErrors);
-    hiddenGradients.scale(this.lr);
-    const inputsTransposed = Matrix.transpose(inputsMatrix);
-    const weightsIHDeltas = Matrix.mult(hiddenGradients, inputsTransposed);
-
-    // Update the weights of the hidden layer
-    this.weightsIH.addMatrix(weightsIHDeltas);
-
-    // Update the biases of the hidden layer
-    this.biasIH.addMatrix(hiddenGradients);
+    for (let l = 0; l < this.weights.length; l++) {
+      m = Matrix.mult(this.weights[l], m);
+      m.addMatrix(this.biases[l]);
+      m.map(sigmoid);
+    }
+    
+    return m.toArray().reduce((accumulator, value) => accumulator.concat(value), []);
   }
 }
